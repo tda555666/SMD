e469967c9170bf350b7491549f3ab4a4a8244b52
tda555666 <mtb4ever@protonmail.com>
2024-08-26 15:54:02 +0300

    1

--- client/src/components/Cards/ProfileInfo.jsx
+++ client/src/components/Cards/ProfileInfo.jsx
@@ -1,24 +1,49 @@
 import { getInitials } from '@/utils/helper';
 import PropTypes from 'prop-types';
+import LoginContext from '@/context/loginContext';
+import { useContext } from "react";
+
+
 
 function ProfileInfo({ onLogout }) {
-    // return active user to guest , set user guest
-    // delete tokens from the storage , and in the server do a logout 
+    const { currUser } = useContext(LoginContext);
+    console.log(currUser);
+    let name = currUser.username ? currUser.username : "";
     ProfileInfo.propTypes = {
         onLogout: PropTypes.func.isRequired,
     };
     return (
-        <div className="flex items-center gap-3">
-            <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">{getInitials("Eugene Krauss")}</div>
+      <>
+        {name ? (
+          <div className="flex items-center gap-3">
+            <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
+              {getInitials(name)}
+            </div>
 
             <div>
-                <p className="text-sm font-medium">Eugene Krauss</p>
-                <button className="text-sm text-slate-700 underline" onClick={onLogout}>
-                    Logout
-                </button>
+              <p className="text-sm font-medium">{name}</p>
+              <button
+                className="text-sm text-slate-700 underline"
+                onClick={onLogout}
+              >
+                Logout
+              </button>
             </div>
-        </div>
-    )
+          </div>
+        ) : (
+          <div className="flex items-center gap-3">
+            <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100"></div>
+
+            <div>
+              <p className="text-sm font-medium">{name}</p>
+              <button className="text-sm text-slate-700 underline">
+                <a href="/login">Login</a>
+              </button>
+            </div>
+          </div>
+        )}
+      </>
+    );
 }
 
 export default ProfileInfo;
